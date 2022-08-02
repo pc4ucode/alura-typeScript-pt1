41a102e00db55560414ca13a8f730f52a17b4daa
euler <euler.amorim@hotmail.com>
2022-08-02 18:08:03 -0300

    aula 4 - Surpresa ao instanciar uma negociacao

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,23 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
-
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
-
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
-
     adiciona() {
-        const negociacao = new Negociacao(
-            this.inputData.value,
-            this.inputQuantidade.value,
-            this.inputValor.value
-        );
-
+        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         console.log(negociacao);
     }
 }

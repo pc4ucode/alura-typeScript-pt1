a9ed64759bfff5702df99f143cc7e6efbc17f6ce
euler <euler.amorim@hotmail.com>
2022-08-10 22:27:14 -0300

    comit do capitulo final

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -14,7 +14,9 @@ export class NegociacaoController {
     }
 
     adiciona(): void {   
-        const negociacao = this.criaNegociacao();     
+        const negociacao = this.criaNegociacao();   
+        negociacao.data.setDate(12);
+
         this.negociacoes.adiciona(negociacao);        
         console.log(this.negociacoes.lista());
         this.limparFormulario();
